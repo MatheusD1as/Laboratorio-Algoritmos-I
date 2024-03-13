69c846d440063e6e8c76350b625d57fc1bb90bf4
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-13 15:24:28 -0300

    Create exercicio4.py

---
+++ Semana03-Variaveis/exercicio4.py
@@ -0,0 +1,5 @@
+valorPneu = 395.40
+pneuPorCarro = valorPneu * 4
+carros = int(input("Digite o total de carros:"))
+valorFinal = carros * pneuPorCarro
+print("O valor final a ser gasto com pneus será de:",valorFinal)
