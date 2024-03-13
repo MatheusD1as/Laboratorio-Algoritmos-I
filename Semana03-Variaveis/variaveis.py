21c815678a4d8a3f80eb8d62298085cf5a615550
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-12 21:09:58 -0300

    Create variaveis.py

---
+++ Semana03-Variaveis/variaveis.py
@@ -0,0 +1,20 @@
+idade = 20
+nome = 'Matheus Dias'
+cpf = '000.000.000-00'
+peso = 70
+dataDeNascimento = '06/11/2003'
+cidade = 'Cachoeira do Sul'
+estado = 'Rio Grande do Sul'
+velocidade = 100
+preco = 1000000
+print(
+    'Sua idade é',idade,
+    '\nSeu nome é',nome,
+    '\nSeu cpf é',cpf,
+    '\nSeu peso é',peso,
+    '\nVocê nasceu no dia',dataDeNascimento,
+    '\nSua cidade natal é',cidade,
+    '\nSeu estado é o',estado,
+    '\nVocê está a',velocidade,'Km/h'
+    '\nO preço é de',preco,
+)
