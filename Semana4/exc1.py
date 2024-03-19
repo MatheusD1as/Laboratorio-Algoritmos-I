5316f754642c182fff7b101701818f643da2e002
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-19 20:26:42 -0300

    Update exc1.py

--- Semana4/exc1.py
+++ Semana4/exc1.py
@@ -1,1 +1,8 @@
-teste
+sph = 35
+hrs = float(input("Digite o total de horas que você trabalha no mês:"))
+slrM = 35 * hrs
+if slrM < 1000:
+    slrAumentado = slrM + 300
+    print("Seu salário mensal é de:", slrAumentado)
+else:
+    print("Seu salário mensal é de:", slrM)
