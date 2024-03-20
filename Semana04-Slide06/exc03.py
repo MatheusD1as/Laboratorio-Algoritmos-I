a075c9c2b98f51625a8b6aae2d4ad49e8e2e4900
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-20 14:59:50 -0300

    Create exc03.py

---
+++ Semana04-Slide06/exc03.py
@@ -0,0 +1,6 @@
+alternativa = input("Qual alternativa foi assinalada 'A', 'B', 'C' ou 'D':").upper()
+if alternativa == "B":
+    print("Alternativa correta")
+else:
+    print("Alternativa incorreta")
+     
