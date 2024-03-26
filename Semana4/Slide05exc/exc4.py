bf87c0d764b9738bce5708db43e008f4db6e7a64
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 13:55:32 -0300

    Create exc4.py

---
+++ Semana4/Slide05exc/exc4.py
@@ -0,0 +1,3 @@
+pLGasolina = float(input("Digite o preço que está o Litro da gasolina:"))
+vlrPgmt = float(input("Digite o valor que irá pagar:"))
+print("Você conseguiu botar", vlrPgmt/pLGasolina,"L de gasolina")
