1b46557ebe2f27a9e975d798fe030f4336a2199a
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 14:30:21 -0300

    Delete Semana4/exc4.py

--- Semana4/exc4.py
+++
@@ -1,3 +0,0 @@
-pLGasolina = float(input("Digite o preço que está o Litro da gasolina:"))
-vlrPgmt = float(input("Digite o valor que irá pagar:"))
-print("Você conseguiu botar", vlrPgmt/pLGasolina,"L de gasolina")
