b19173b29c1448bec12fdef7dd4d7b31dfae4821
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 14:34:25 -0300

    Update exc01.py

--- Semana4/Slides06exc/exc01.py
+++ Semana4/Slides06exc/exc01.py
@@ -1,1 +1,8 @@
-
+n1 = float(input("Digite um número:"))
+n2 = float(input("Digite um número:"))
+if n1 > n2:
+    print(n1,"é o maior e",n2,"é o menor")
+elif n2 > n1:
+    print(n2,"é o maior e",n1,"é o menor")
+else:
+    print("Os números tem o mesmo valor")
