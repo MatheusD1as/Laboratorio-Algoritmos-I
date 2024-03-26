bfcef912d5a8750d8d2103c5c2e3392cd0c0e0a5
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 13:58:28 -0300

    Update and rename Semana4/exc1.py to Semana43

--- Semana4/exc1.py
+++
@@ -1,8 +0,0 @@
-sph = 35
-hrs = float(input("Digite o total de horas que você trabalha no mês:"))
-slrM = 35 * hrs
-if slrM < 1000:
-    slrAumentado = slrM + 300
-    print("Seu salário mensal é de:", slrAumentado)
-else:
-    print("Seu salário mensal é de:", slrM)
