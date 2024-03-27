f3a1ee0c52ab4ca618ad3a6c4fa0a03c2aecfa3e
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 22:25:25 -0300

    Update exc03.py

--- Semana05/exc03.py
+++ Semana05/exc03.py
@@ -1,20 +1,32 @@
 qtdSim = 0
 
-resposta = input("Telefonou para vítima (SIM OU NÃO):").upper()
+resposta = input("Telefonou para vítima?(SIM OU NÃO):").upper()
 if resposta == "SIM":
     qtdSim = qtdSim + 1
 
 
-resposta = input("Esteve no local do crime (SIM OU NÃO):").upper()
+resposta = input("Esteve no local do crime? (SIM OU NÃO):").upper()
 if resposta == "SIM":
     qtdSim = qtdSim + 1
 
 
-resposta = input("Esteve no local do crime (SIM OU NÃO):").upper()
+resposta = input("Mora perto da vítima? (SIM OU NÃO):").upper()
 if resposta == "SIM":
     qtdSim = qtdSim + 1
 
-###### Todas as perguntas
+resposta = input("Devia para vítima? (SIM OU NÃO):").upper()
+if resposta == "SIM":
+    qtdSim = qtdSim + 1
+
+resposta = input("Já trabalhou para vítima? (SIM OU NÃO):").upper()
+if resposta == "SIM":
+    qtdSim = qtdSim + 1
     
-if qtdSim == 2
+if qtdSim == 2:
     print("Suspeito do crime")
+elif qtdSim == 3 or qtdSim == 4:
+    print("Cúmplice do crime")
+elif qtdSim == 5:
+    print("Assasino")
+else:
+    print("Inocente")
