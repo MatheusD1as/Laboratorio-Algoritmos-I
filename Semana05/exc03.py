b53e501d40d67af5096962902d691cbecabf2758
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 22:12:48 -0300

    Create exc03.py

---
+++ Semana05/exc03.py
@@ -0,0 +1,20 @@
+qtdSim = 0
+
+resposta = input("Telefonou para vítima (SIM OU NÃO):").upper()
+if resposta == "SIM":
+    qtdSim = qtdSim + 1
+
+
+resposta = input("Esteve no local do crime (SIM OU NÃO):").upper()
+if resposta == "SIM":
+    qtdSim = qtdSim + 1
+
+
+resposta = input("Esteve no local do crime (SIM OU NÃO):").upper()
+if resposta == "SIM":
+    qtdSim = qtdSim + 1
+
+###### Todas as perguntas
+    
+if qtdSim == 2
+    print("Suspeito do crime")
