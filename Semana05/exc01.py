9745b9bf9457ff16eae5bb6f8c1dd4d6181da57a
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-26 21:57:00 -0300

    Update exc01.py

--- Semana05/exc01.py
+++ Semana05/exc01.py
@@ -1,1 +1,8 @@
+idade = int(input("Digite a sua idade:"))
 
+if idade >= 18 and idade < 70:
+    print("Obrigado a votar")
+elif idade >= 16 or idade > 70:
+    print("Voto facultativo")
+else:
+    print("Não pode votar")
