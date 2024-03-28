cf43dc8616a4c2fd2d26340c3eae6573494430a3
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-28 13:20:07 -0300

    Update exc02.py

--- Semana05/exc02.py
+++ Semana05/exc02.py
@@ -1,4 +1,13 @@
-letra = input("Digite uma letra:").upper()
-if letra == "A" or letra == "E"  or letra == "I" or letra == "O" or letra == "U":
-    print("É Vogal")
-    
+A = int(input("Digite o lado A do triângulo:"))
+B = int(input("Digite o lado B do triângulo:"))
+C = int(input("Digite o lado C do triângulo:"))
+if A+B > C and C+A > B and C+B > A:
+  print("O triângulo é verdadeiro")
+  if (A == B) and (C == A) :
+    print("É um triângulo verdadeiro e equilátero")
+  elif (A != B) and (C != A) and (C != B):
+    print("É um triângulo verdadeiro escaleno")
+  else:
+    print("é um triângulo isóceles")
+else:
+  print("não é um triângulo verdadeiro")
