06c9e3dc6ecb864e085d884f0d94ab0c76c8a78c
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-03-28 13:14:14 -0300

    Update exc01.py

--- Semana05/exc01.py
+++ Semana05/exc01.py
@@ -1,8 +1,12 @@
-idade = int(input("Digite a sua idade:"))
-
-if idade >= 18 and idade < 70:
-    print("Obrigado a votar")
-elif idade >= 16 or idade > 70:
-    print("Voto facultativo")
+nome = input("Digite o nome do Funcionário:")
+slrio = float(input("Digite o salário do funcionário:"))
+tmpSvc = int(input("Digite o tempo de serviço do funcionário em anos:"))
+if tmpSvc >= 5:
+    if slrio <= 2000:
+        slrio *= 1.1
+        print("O funcionário",nome,"tem direito a aumento de 10'%' do salário ficando com um salário final de",slrio,)
+    elif slrio > 2000:
+        slrio *= 1.05
+        print("O funcionário",nome,"tem direito a aumento de 5 '%' do salário ficando com um salário final de",slrio,)
 else:
-    print("Não pode votar")
+    print("O funcionário ainda não tem tempo suficiente de serviço para receber o aumento")
