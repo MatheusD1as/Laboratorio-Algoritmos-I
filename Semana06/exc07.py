0592fa4c35dc562717f8b6f7bdbdf3d4c49fc4d9
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-04-05 16:10:30 -0300

    Create exc07.py

---
+++ Semana06/exc07.py
@@ -0,0 +1,8 @@
+qtdN = 0
+n30a90 = 0
+while qtdN < 10:
+    numero = float(input(""))
+    qtdN += 1
+    if numero >= 30 and numero <= 90:
+        n30a90 += 1
+print("Quantidade entre 30 e 90:",n30a90)
