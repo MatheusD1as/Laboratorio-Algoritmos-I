1ad09567a3bc9ed8bf9a98241269f0129bb9732d
MatheusD1as <161779583+MatheusD1as@users.noreply.github.com>
2024-04-12 15:51:18 -0300

    Create Exc04.py

---
+++ Semana07/Exc04.py
@@ -0,0 +1,9 @@
+meuNumero = 44
+n = 0
+while n != 44:
+    n = int(input("Digite um número inteiro de 1 a 100:"))
+    if n < 44:
+        print("Digite um número maior")
+    elif n > 44:
+        print("Digite um número menor")
+print("Parabéns")
